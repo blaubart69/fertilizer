8b07c376de6c8a51353ce4e40a35b993c615f5dd
Yeannie <berni@spind>
2021-04-24 09:57:45 +0200

    important changes im Stodl

--- calc.py
+++ calc.py
@@ -6,7 +6,7 @@ import calcgpio
 DuengerRatio = {
       "Kali"      : ( 30, 6.1  )
     , "Harnstoff" : ( 30, 4.0  )
-    , "Phosphor"  : ( 30, 4.95 )
+    , "Phosphor"  : ( 30, 5.7 )
     , "KAS"       : ( 30, 5.55 )
 }
 
@@ -20,7 +20,7 @@ _bufRoller = buffer.SignalBuf(_bufSizeSignals)
 _timespanMillisToWatch = 5000
 
 wheel_meter=50
-wheel_signals=377
+wheel_signals=417
 _signals_per_meter = wheel_signals / wheel_meter
 
 _signals_per_kilo =  0
@@ -53,16 +53,16 @@ def reset():
     overallKilo = 0
 
 def checkSignals(cntWheel, cntRoller):
-    cntBadSignal = 0
+    #cntBadSignal = 0
 
     if cntWheel == 0:
         print("E: no signals from wheel")
-        cntBadSignal += 1
+        #cntBadSignal += 1
     if cntRoller == 0:
         print("E: no signals from roller")
-        cntBadSignal += 1
+        #cntBadSignal += 1
 
-    return (cntBadSignal == 0)
+    #return (cntBadSignal == 0)
 
 def current():
     global overallMeter, overallKilo, _lastMillis
@@ -70,16 +70,20 @@ def current():
 
     signalsWheel  = _bufWheel.getSignalsWithinTimespan(timestampNow=currentMillis,  timespan=_timespanMillisToWatch)
     signalsRoller = _bufRoller.getSignalsWithinTimespan(timestampNow=currentMillis, timespan=_timespanMillisToWatch)
-    print("signals wheel: {}\tsignals roller: {}".format(signalsWheel, signalsRoller))
+    print("signals wheel: {}\tsignals roller: {}, signal wheel overall: {}".format(signalsWheel, signalsRoller, _bufWheel.rbuf.overallSignals))
 
-    if checkSignals(signalsWheel, signalsRoller) == False:
-        return 0,0,0
+    #if checkSignals(signalsWheel, signalsRoller) == False:
+    #    return 0,0,0
+    checkSignals(signalsWheel, signalsRoller)
 
     meters_in_timespan = signalsWheel  / _signals_per_meter
     kilos_in_timespan  = signalsRoller / _signals_per_kilo
 
-    kilos_per_meter = kilos_in_timespan / meters_in_timespan
-    kilo_per_ha     = kilos_per_meter * METERS_PER_HEKTAR
+    if meters_in_timespan > 0:
+        kilos_per_meter = kilos_in_timespan / meters_in_timespan
+        kilo_per_ha     = kilos_per_meter * METERS_PER_HEKTAR
+    else:
+        kilo_per_ha = 0
 
     if _lastMillis != 0:
         millis_diff = currentMillis - _lastMillis   # 2,5s
